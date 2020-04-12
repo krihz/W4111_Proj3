0f9b614d46a70b988f1209e5c00e6dc3391ccb04
krihz <cherryhhz@outlook.com>
2020-04-12 15:08:18 -0400

    Update server.py

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ db = SQLAlchemy(app)
 #
 #     DATABASEURI = "postgresql://gravano:foobar@35.243.220.243/proj1part2"
 #   35.231.103.173
-DATABASEURI = "postgresql://hh2816:5783@34.74.50.234/proj1part2"
+DATABASEURI = "postgresql://hh2816:5783@35.231.103.173/proj1part2"
 
 
 #
