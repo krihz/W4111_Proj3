461dfeb680ec5792027cbd81604bd539d41684ab
krihz <cherryhhz@outlook.com>
2020-04-13 14:07:50 -0400

    Update server.py

--- server.py
+++ server.py
@@ -169,10 +169,10 @@ def Food_calorie():
 def Exercise():
   name = request.form['name']
   username = session.get('username')
-  name = name + '%'
-  s = text("Exercise_Diary.exercise_name|| ', ' || Exercise_Diary.calories|| ', ' || Exercise_Diary.date_time FROM Exercise_Diary, Register"
+  name = name
+  s = text("Select Exercise_Diary.exercise_name|| ', ' || Exercise_Diary.calories|| ', ' || Exercise_Diary.date_time FROM Exercise_Diary, Register"
            "WHERE Exercise_Diary.id =Register.id"
-           "AND Register.username = :x1 AND TO_CHAR(Exercise_Diary.date_time) LIKE :x2")
+           "AND Register.username = :x1 AND TO_CHAR(Exercise_Diary.date_time, 'YYYY-MM-DD') = :x2")
   cursor = g.conn.execute(s, x1=username, x2=name).fetchall()
   if (name != ''):
     names = []
