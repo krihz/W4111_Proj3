6c9373ec31180ae7233e39a24b8ba4417a2c17b7
krihz <cherryhhz@outlook.com>
2020-04-13 22:30:55 -0400

    Update server.py

--- server.py
+++ server.py
@@ -291,7 +291,7 @@ def BMI():
     height = g.conn.execute("SELECT Height FROM user_info WHERE id = %(user_id)s",{'user_id':user_id})
     height = height.first()[0]
     BMI = weight/(height*height)
-    names = [['BMI'],int(BMI)]
+    names = [['BMI'],[BMI]]
     context = dict(data = names)
     return render_template("index.html", **context)
 
